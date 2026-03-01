dcc3dc50601b38f9aa3d00233bd7ed21e7dc95e1
Jones. <jonesmailhub@gmail.com>
2026-03-01 07:54:02 +0500

    Create 0961.py

---
+++ 0961.py
@@ -0,0 +1,56 @@
+from random import choice, randint
+
+# Переменные
+alphabet: str = "йцукенгшщзхфывапролджэячсмитьбю"
+alphabet_red: str = "еыаоэяь"
+text: str = ""
+word: str = ""
+line: str = "·" * 25
+    
+# Генератов слова
+for size in range(2, 10 + 1):
+    
+    # Первая буква слова
+    while True:
+        word += choice(alphabet)
+        if "ь" not in word:
+            break
+        else:
+            word = ""
+            
+    # Остальные буквы слова
+    size_word = randint(2, 10)
+    while not False:
+        if len(word) == size_word:
+            break
+        else:
+            choice_letter = choice(alphabet)
+            if word[-1] in alphabet_red:
+                if choice_letter not in alphabet_red:
+                    if word[-1].lower() != choice_letter:
+                        word += choice_letter
+            else:
+                if choice_letter in alphabet_red:
+                    if word[-1].lower() != choice_letter:
+                        word += choice_letter
+                    
+    # Добавление в текст
+    text += f" {word}"
+    word = ""
+
+# Делаем первую букву заглавной
+cap = text[1].upper()
+text = " " + cap + text[2:]
+
+# Показываем в консоли
+print(line)
+counter: int = 0
+for sym in text:
+    if sym == " ":
+        counter += 1
+    if counter == 3:
+        print(",")
+        counter = 0
+    print(sym, end="")
+print(".")
+print(line)
